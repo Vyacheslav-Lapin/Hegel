08ec761468811bb474acdaf51f49623ac2abaff2
Vyacheslav Lapin <Vyacheslav_Lapin@epam.com>
2016-02-12 02:32:29 +0300

    Added getMethods functionality.

--- Reflect/src/test/java/com/hegel/reflect/TestClass.java
+++ Reflect/src/test/java/com/hegel/reflect/TestClass.java
@@ -1,13 +1,5 @@
-/*
- * Canadian Tire Corporation, Ltd. Do not reproduce without permission in writing.
- * Copyright (c) 2016 Canadian Tire Corporation, Ltd. All rights reserved.
- */
-
 package com.hegel.reflect;
 
-/**
- * @author TestClass
- */
 public class TestClass {
     private String string = "мама мыла раму";
     volatile int anInt = 5;
