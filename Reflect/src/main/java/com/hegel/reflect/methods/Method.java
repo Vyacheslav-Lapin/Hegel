1de07d6262e3ba07981924391efbe591f0941c71
Vyacheslav Lapin <Vyacheslav_Lapin@epam.com>
2016-02-14 23:48:58 +0300

    bug fix

--- Reflect/src/main/java/com/hegel/reflect/methods/Method.java
+++ Reflect/src/main/java/com/hegel/reflect/methods/Method.java
@@ -1,5 +1,7 @@
 package com.hegel.reflect.methods;
 
+import com.hegel.reflect.Class;
+
 @FunctionalInterface
 public interface Method<C> {
 
@@ -7,12 +9,15 @@ public interface Method<C> {
 
     @SuppressWarnings("unchecked")
     static <C, M extends Method<C>> M wrap(java.lang.reflect.Method method) {
-        Class<?> type = method.getReturnType();
+        java.lang.Class<?> type = method.getReturnType();
         return (M) (type == int.class || type == short.class || type == char.class || type == byte.class ? IntMethod.wrap(method) :
                 type == long.class ? LongMethod.wrap(method) :
                         type == double.class || type == float.class ? DoubleMethod.wrap(method) :
                                 ObjectMethod.wrap(method));
     }
 
-
+    @SuppressWarnings("unchecked")
+    default <T extends C> Class<T> getType() {
+        return Class.wrap((T) toSrc().getDeclaringClass());
+    }
 }
--- Reflect/src/test/java/com/hegel/reflect/TestClass.java
+++ Reflect/src/test/java/com/hegel/reflect/TestClass.java
@@ -1,9 +1,13 @@
 package com.hegel.reflect;
 
 public class TestClass {
+
     private String string = "мама мыла раму";
+
     volatile int anInt = 5;
+
     public static final double PI = Math.PI;
+
     public static Integer counter;
 
     public String getString() {
